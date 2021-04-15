9906e0f0902efac22d12eb76733196199568cd94
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-04-14 22:24:09 -0400

    Add scoring tests

---
+++ tests/test_scoring.py
@@ -0,0 +1,40 @@
+"""Test scoring functions."""
+from src.scoring import score
+
+
+def test_scoring():
+    """Test scoring."""
+    pattern = "ab.*"
+    inlist = [
+        "abbot",
+        "absolute",
+    ]
+    outlist = [
+        "rabbit",
+        "alba",
+    ]
+    assert score(pattern, inlist, outlist) == {
+        "true_positives": 2,
+        "true_negatives": 2,
+        "false_positives": 0,
+        "false_negatives": 0,
+        "accuracy": 1.0,
+        "length": 4,
+    }
+    pattern = "ab.*"
+    inlist = [
+        "abbot",
+        "rabbit",
+    ]
+    outlist = [
+        "absolute",
+        "alba",
+    ]
+    assert score(pattern, inlist, outlist) == {
+        "true_positives": 1,
+        "true_negatives": 1,
+        "false_positives": 1,
+        "false_negatives": 1,
+        "accuracy": 0.5,
+        "length": 4,
+    }
